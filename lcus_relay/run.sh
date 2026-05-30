dd139d7b95c3d583d5b8c81ff071602fabd7151e
stegno <38944977+stegno@users.noreply.github.com>
2026-05-29 23:32:56 -0600

    Update run.sh

--- lcus_relay/run.sh
+++ lcus_relay/run.sh
@@ -3,4 +3,9 @@
 export SERIAL_PORT=$(bashio::config 'serial_port')
 export POLL_INTERVAL=$(bashio::config 'poll_interval')
 
+export MQTT_HOST=$(bashio::services mqtt "host")
+export MQTT_PORT=$(bashio::services mqtt "port")
+export MQTT_USER=$(bashio::services mqtt "username")
+export MQTT_PASSWORD=$(bashio::services mqtt "password")
+
 python3 /app/lcus.py
